d99d48a127ea73f3a0bf53928e16bf618b829ddd
Kai Bao <kai.bao@sintef.no>
2019-02-27 14:47:31 +0100

    using B_avg for the inner iteration for multisegment wells

--- opm/autodiff/BlackoilWellModel_impl.hpp
+++ opm/autodiff/BlackoilWellModel_impl.hpp
@@ -652,6 +652,9 @@ namespace Opm {
         // Set the well primary variables based on the value of well solutions
         initPrimaryVariablesEvaluation();
 
+        std::vector< Scalar > B_avg(numComponents(), Scalar() );
+        computeAverageFormationFactor(B_avg);
+
         if (param_.solve_welleq_initially_ && iterationIdx == 0) {
             // solve the well equations as a pre-processing step
             last_report_ = solveWellEq(dt, local_deferredLogger);
@@ -839,10 +842,6 @@ namespace Opm {
     solveWellEq(const double dt, Opm::DeferredLogger& deferred_logger)
     {
         WellState well_state0 = well_state_;
-
-        const int numComp = numComponents();
-        std::vector< Scalar > B_avg( numComp, Scalar() );
-        computeAverageFormationFactor(B_avg);
 
         const int max_iter = param_.max_welleq_iter_;
 
@@ -1381,7 +1380,7 @@ namespace Opm {
     template<typename TypeTag>
     void
     BlackoilWellModel<TypeTag>::
-    computeAverageFormationFactor(std::vector<double>& B_avg) const
+    computeAverageFormationFactor(std::vector<Scalar>& B_avg) const
     {
         const auto& grid = ebosSimulator_.vanguard().grid();
         const auto& gridView = grid.leafGridView();
