ce399ad2a2556ce3a5e78ed1c3a0c16a0f5c3a38
Kai Bao <kai.bao@sintef.no>
2022-12-16 00:42:40 +0100

    extending WINJMULT support to MultisegmentWell
    not sure whether it is the correct form to go yet.

--- opm/simulators/wells/BlackoilWellModel_impl.hpp
+++ opm/simulators/wells/BlackoilWellModel_impl.hpp
@@ -481,7 +481,6 @@ namespace Opm {
                 well->updateWaterThroughput(dt, this->wellState());
             }
             if (well->isInjector()) {
-                // well->updateInjMult(this->wellState().well(well->indexOfWell()));
                 well->updateInjMult(this->inj_multipliers_[well->name()]);
             }
         }
