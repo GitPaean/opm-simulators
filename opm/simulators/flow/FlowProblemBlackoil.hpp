66580ec0c321a27fe385ef128a014133080a6141
Kai Bao <kai.bao@sintef.no>
2025-05-13 16:22:24 +0200

    compile while not linkable yet.

--- opm/simulators/flow/FlowProblemBlackoil.hpp
+++ opm/simulators/flow/FlowProblemBlackoil.hpp
@@ -1677,7 +1677,7 @@ protected:
 #endif
     MixingRateControls<FluidSystem> mixControls_;
 
-    ActionHandler<Scalar> actionHandler_;
+    ActionHandler<FluidSystem, Indices> actionHandler_;
 
     ModuleParams moduleParams_;
 
