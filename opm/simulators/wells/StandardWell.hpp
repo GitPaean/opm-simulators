7deaddc8d89d25e1ba09f4b4caef1907e2b53d76
Kai Bao <kai.bao@sintef.no>
2025-07-30 16:33:03 +0200

    WIP in adding the energy equations for the StandardWell

--- opm/simulators/wells/StandardWell.hpp
+++ opm/simulators/wells/StandardWell.hpp
@@ -88,6 +88,8 @@ namespace Opm
         using Base::has_bioeffects;
         using Base::has_micp;
 
+        using typename StdWellEval::PrimaryVariables;
+
         using PolymerModule =  BlackOilPolymerModule<TypeTag>;
         using FoamModule = BlackOilFoamModule<TypeTag>;
         using typename Base::PressureMatrix;
@@ -454,7 +456,7 @@ namespace Opm
                                 DeferredLogger& deferred_logger) const;
 
     private:
-        Eval connectionRateEnergy(const std::vector<EvalWell>& cq_s,
+        EvalWell connectionRateEnergy(const std::vector<EvalWell>& cq_s,
                                   const IntensiveQuantities& intQuants,
                                   DeferredLogger& deferred_logger) const;
 
