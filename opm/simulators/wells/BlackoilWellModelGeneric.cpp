53e5a18c459e2c4cdb7dd1ed3396bc38ed920342
Kai Bao <kai.bao@sintef.no>
2025-11-27 23:42:43 +0100

    outputting the information when an action open a well

--- opm/simulators/wells/BlackoilWellModelGeneric.cpp
+++ opm/simulators/wells/BlackoilWellModelGeneric.cpp
@@ -854,6 +854,12 @@ updateEclWellsConstraints(const int              timeStepIdx,
                          (const auto wellIdx, const auto& well)
     {
         auto& ws = this->wellState().well(wellIdx);
+        if (ws.status == WellStatus::SHUT) {
+            std::string msg = fmt::format(" well {} is SHUT, but an action will make it status to be {} at rank {}",
+                                          well.name(), WellStatus2String(well.getStatus()), comm_.rank());
+            std::cout << msg << std::endl;
+            OpmLog::info(msg);
+        }
         ws.updateStatus(well.getStatus());
         ws.update_type_and_targets(well, st);
     });
