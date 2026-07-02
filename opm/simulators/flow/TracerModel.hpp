2593d0d75f3a103a62a9a5e55b3e22ad61cebd18
Kai Bao <kai.bao@sintef.no>
2026-07-02 15:15:46 +0200

    avoid per-iteration copy of element chunks by using const reference

--- opm/simulators/flow/TracerModel.hpp
+++ opm/simulators/flow/TracerModel.hpp
@@ -628,7 +628,7 @@ protected:
             #pragma omp parallel for
             #endif
             for (std::size_t ci = 0; ci < element_chunks_.size(); ++ci) {
-                const auto chunk = element_chunks_[ci];
+                const auto& chunk = element_chunks_[ci];
                 ElementContext elemCtx(simulator_);
                 const Scalar dt = elemCtx.simulator().timeStepSize();
 
@@ -735,7 +735,7 @@ protected:
         #pragma omp parallel for
         #endif
         for (std::size_t ci = 0; ci < element_chunks_.size(); ++ci) {
-            const auto chunk = element_chunks_[ci];
+            const auto& chunk = element_chunks_[ci];
             ElementContext elemCtx(simulator_);
 
             for (const auto& elem : chunk) {
